844d1bf7b9b231bf5b016709c60091c6884d6d2a
Jun <jun.lazaro02@gmail.com>
2022-12-20 22:36:50 +0800

    add config token and proxy

--- src/js/config.js
+++ src/js/config.js
@@ -1,2 +1,5 @@
 export const proxy = "https://cors-anywhere.herokuapp.com/";
 export const key = "414b174427112a458f00fca2cb54ff46"; //API key
+export const Food2ForkToken = "Token 9c8b06d329136da358c2d00e76946b0111ce2c48";
+export const proxy1 = "https://thingproxy.freeboard.io/fetch/";
+export const proxy2 = "http://alloworigin.com/get?url=";
