1a6d29b23d1eb961d85bfa92d7b53804870422b5
Jun <jun.lazaro02@gmail.com>
2022-12-20 22:43:19 +0800

    Update on API get request

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,5 +1,5 @@
 import axios from "axios"; // importing package is not using a Path only package name
-import { key } from "../config";
+import { Food2ForkToken, proxy } from "../config";
 
 export default class Search {
   constructor(query) {
@@ -7,15 +7,20 @@ export default class Search {
   }
   async getResults() {
     //error handling
-    try {
-      const res = await axios(
-        `https://www.food2fork.com/api/search?key=${key}&q=${this.query}`
-      ); // axios is same as fetch but it work in all of the browser
-      //console.log(res);
-      this.result = res.data.recipes;
-      //console.log(this.result);
-    } catch (error) {
-      alert(error);
-    }
+
+    axios({
+      method: "get",
+      url: `/search/?page=1&query=${this.query}`,
+      baseURL: `${proxy}https://food2fork.ca/api/recipe`,
+      headers: {
+        Authorization: Food2ForkToken,
+        "Access-Control-Allow-Origin": "*",
+      },
+    })
+      .then((result) => {
+        console.log("REsult", result);
+        this.result = result.data.results;
+      })
+      .catch((err) => alert(err));
   }
 }
